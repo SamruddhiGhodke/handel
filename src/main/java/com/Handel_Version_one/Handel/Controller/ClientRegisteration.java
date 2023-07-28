7820b037b3bf2a5c5053a9c1ebb489f470aa760a
Your Name <samruddhi@integritymatters.in>
2023-07-28 14:45:13 +0530

    first _api

---
+++ src/main/java/com/Handel_Version_one/Handel/Controller/ClientRegisteration.java
@@ -0,0 +1,46 @@
+package com.Handel_Version_one.Handel.Controller;
+
+import com.Handel_Version_one.Handel.Entity.RegistrationEntity;
+import com.Handel_Version_one.Handel.Model.LoginModel;
+import com.Handel_Version_one.Handel.Repository.ClientRegi;
+import com.Handel_Version_one.Handel.Service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+@CrossOrigin(origins = "http://localhost:4200",maxAge = 3600)
+@RestController
+
+@RequestMapping("/client")
+public class ClientRegisteration {
+    @Autowired
+    ClientService clientService;
+
+    @Autowired
+    private ClientRegi clientRegi;
+    @PostMapping("/registration")
+    public RegistrationEntity addData(@RequestBody RegistrationEntity regi)
+    {
+
+        return this.clientService.addClientService(regi);
+    }
+
+        @PostMapping("/login")
+        public ResponseEntity<String> login(@RequestBody LoginModel loginModel) {
+            String emailId = loginModel.getEmailId();
+            String password = loginModel.getPassword();
+
+            RegistrationEntity user = clientRegi.findByEmailId(emailId);
+
+            if (user != null && password.equals(user.getPassword())) {
+                // Authentication successful
+                return ResponseEntity.ok("Login successful!");
+            } else {
+                // Authentication failed
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+            }
+        }
+    }
+
+
+
---
+++ src/main/java/com/Handel_Version_one/Handel/Model/LoginModel.java
@@ -0,0 +1,23 @@
+package com.Handel_Version_one.Handel.Model;
+
+public class LoginModel {
+    private String emailId;
+    private String password;
+
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
